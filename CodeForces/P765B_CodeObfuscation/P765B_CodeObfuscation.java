ab44840fc628975d068e153d724ac2e2d455b6bf
George Beshay <georgebeshay101@gmail.com>
2022-07-16 11:14:15 +0200

    Add files via upload

---
+++ CodeForces/P765B_CodeObfuscation/P765B_CodeObfuscation.java
@@ -0,0 +1,62 @@
+/*
+ * Problem Link:			https://codeforces.com/problemset/problem/765/B
+ * Problem Status:		Accepted.
+ */
+
+package CodeForces.P765B_CodeObfuscation;
+
+import java.util.*;
+
+public class P765B_CodeObfuscation {
+	
+	public static String SolverA(String Input) {
+		// --------------------------------------- SEPARATOR ---------------------------------------
+		boolean Flag = false;
+		int[] Arr = new int[26];
+		int LastVisited = 96;
+		// --------------------------------------- SEPARATOR ---------------------------------------
+		for(int i = 0 ; i < Input.length() ; i++) {
+			// --------------------------------------- SEPARATOR ---------------------------------------
+			char tempChar = Input.charAt(i);
+			// --------------------------------------- SEPARATOR ---------------------------------------
+			if((int)tempChar <= LastVisited) {
+				if(Arr[(int)tempChar - 97] == 1)
+					continue;
+				else {
+					Flag = true;
+					break;
+				}
+			}
+			// --------------------------------------- SEPARATOR ---------------------------------------
+			else {
+				if((int)tempChar - LastVisited == 1) {
+					LastVisited = (int)tempChar;
+					Arr[(int)tempChar - 97] = 1;
+				}
+				else {
+					Flag = true;
+					break;
+				}
+			}
+		}
+		// --------------------------------------- SEPARATOR ---------------------------------------
+		if(!Flag)
+			return "YES";
+		else
+			return "NO";
+		// --------------------------------------- SEPARATOR ---------------------------------------
+	}
+
+	public static void main(String[] args) {
+		// --------------------------------------- SEPARATOR --------------------------------------- 
+		Scanner Reader = new Scanner(System.in);
+		// --------------------------------------- SEPARATOR ---------------------------------------
+		String Input = Reader.next();
+		// --------------------------------------- SEPARATOR ---------------------------------------
+		Reader.close();
+		// --------------------------------------- SEPARATOR ---------------------------------------
+		System.out.println(SolverA(Input));
+		// --------------------------------------- SEPARATOR ---------------------------------------
+	}
+
+}
