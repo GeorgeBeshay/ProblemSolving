867c70b81c53ad7bf19bac38c924f10a34e714ad
George Beshay <georgebeshay101@gmail.com>
2022-10-07 11:18:02 +0200

    Add files via upload

---
+++ CF_Phase_1_1/BasicOperator/P964A_Splits.cpp
@@ -0,0 +1,19 @@
+//
+// Created by George on 10/7/2022.
+//
+
+/*
+ * Problem Link:        https://codeforces.com/problemset/problem/964/A
+ * Problem Status:      Accepted.
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    // ---------- Separator ----------
+    int N;
+    cin >> N;
+    cout << 1 + int(N / 2);
+    // ---------- Separator ----------
+    return 0;
+}
