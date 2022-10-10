1168867c9554be71a0bb9c1fc51089dbd6ff2ce7
George Beshay <georgebeshay101@gmail.com>
2022-10-10 18:37:19 +0200

    Add files via upload

---
+++ CF_Phase_1_1/BasicOperator/P50A_DominoPiling.cpp
@@ -0,0 +1,17 @@
+//
+// Created by George on 10/10/2022.
+//
+
+/*
+ * Problem Link:            https://codeforces.com/problemset/problem/50/A
+ * Problem Status:          Accepted.
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N, M;
+    cin >> N >> M;
+    cout << (N*M) / 2 << endl;
+    return 0;
+}
