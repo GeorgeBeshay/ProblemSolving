803799fa0e135e0737f004c7559e0956786ae2ab
George Beshay <georgebeshay101@gmail.com>
2022-11-06 21:17:01 +0200

    Add files via upload

---
+++ CF_Phase_1_1/Condition/P378A_PlayingWithDice.cpp
@@ -0,0 +1,29 @@
+//
+// Created by George on 11/6/2022.
+//
+
+/*
+ * Problem Link:        https://codeforces.com/problemset/problem/378/A
+ * Problem Status:
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B, F, D, S;
+    F = 0;
+    D = 0;
+    S = 0;
+    cin >> A >> B;
+    for(int i = 1 ; i < 7 ; i++){
+        if(abs(A-i) < abs(B-i))
+            F++;
+        else if(abs(A-i) > abs(B-i))
+            S++;
+        else
+            D++;
+    }
+    cout << F << " " << D << " " << S << endl;
+    return 0;
+}
