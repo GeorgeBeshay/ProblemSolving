c0c1a9bbdab7f30261b91e86a51895931ef550bf
George Beshay <georgebeshay101@gmail.com>
2022-11-10 19:33:17 +0200

    Add files via upload

---
+++ CF_Phase_1_1/Condition/P959A_MahmoudAndEhabAndTheEvenOddGame.cpp
@@ -0,0 +1,20 @@
+//
+// Created by George on 11/10/2022.
+//
+
+/*
+ * Problem Link:        https://codeforces.com/problemset/problem/959/A
+ * Problem Status:      Accepted.
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    if(N % 2 == 0)
+        cout << "Mahmoud" << endl;
+    else
+        cout << "Ehab" << endl;
+    return 0;
+}
